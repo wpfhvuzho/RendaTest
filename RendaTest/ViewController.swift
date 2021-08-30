2d56198ef7a5ef8bb5f1ba78d1d3f3f254cc1b09
Ryu Takahashi <71573078+wpfhvuzho@users.noreply.github.com>
2021-08-30 12:04:31 +0900

    Initial Commit

---
+++ RendaTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RendaTest
+//
+//  Created by 高橋　龍 on 2021/08/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
